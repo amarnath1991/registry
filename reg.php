966b114e9740dfcea8e75cb4e8c23141a0a7e832
Amarnath <amarnath.jammalamadaka@gmail.com>
2013-07-01 13:50:41 +0530

    Registrion for the fest

---
+++ reg.php
@@ -0,0 +1,145 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd"><html><head><link rel="shortcut icon" href="//ssl.gstatic.com/docs/spreadsheets/forms/favicon_jfk.png" type="image/x-icon">
+<meta http-equiv="Content-type" content="text/html; charset=utf-8">
+<title>Registration Form</title>
+
+
+<param name="movie" value="http://thefreelogomakers.com/generators/free-flash-generator/output.swf?gstyle=glitter9.swf&ta=CSESTA 2K12&bgcolor=0x000000&fs=60&sr=2&sa=0.9&sc=16711680&tst=5&rpx1=6.15&rpy1=40&dom=thefreelogomakers.com&swfw=500&swfh=300&pa=http://thefreelogomakers.com/generators/free-flash-generator/"><param name="quality" value="high">
+
+<link href='/static/spreadsheets/client/css/779923916-published_form_compiled.css' type='text/css' rel='stylesheet'>
+
+<style type='text/css'>
+
+textarea {scrollbar-base-color:orange;}  body.ss-base-body{background:#9ca578  url('//lh3.googleusercontent.com/WRXb8KPbJTujqjNuMHLnhX6uqRsW34V9r4NI6RUyhU01aVK39eU6_xNzMH2y3Dyn4PSWqHGJCS01ziSs49ioBuY=s0') top left no-repeat;  color:black;font-size:13pt;font-family:Forte,serif;margin:0px;padding:66px}div.ss-form-container{background:#daddc0 url('//lh3.googleusercontent.com/9Fa7IHYHMfnPfrJvGi-oOE_ivgl_A_GwzX6AWBxRg6xUx2OSN8NQdhyB_Z_HaQJrJAx2_sOdL4-CGUJRPlezGX0=s0') top left no-repeat;border:none;margin:0;padding:10px 66px 20px;width:60em}div.ss-form-entry{background-color:#fafadc ;border:1px solid #bcc2a4 ;margin:0 0 10px;padding:20px}div.errorbox-bad{border:1px solid #666666;margin:0 0 10px;padding:0}div.errorbox-bad div.ss-form-entry{border:none;margin:0}td .ss-scalerow{border-color:#999}h2.ss-section-title{background-color:transparent}div.ss-submit div.ss-form-entry{background:none;border:none}</style></head>
+<body class="ss-base-body" dir="ltr" itemscope itemtype="http://schema.org/CreativeWork/FormObject">
+<img itemprop="image" style="display:none;" src="//ssl.gstatic.com/docs/spreadsheets/forms/forms64.png" alt="Google Forms">
+<div class="ss-form-container">
+
+<div class="ss-form-heading"><h1 class="ss-form-title" itemprop="name"> <center><font color="red">Registration Form</font></h1></center>
+<p></p>
+<form action="register.php" method="POST" >
+
+<hr class="ss-email-break" style="display:none;">
+<div class="ss-required-asterisk">* Required</div></div>
+<div class="ss-form"><form action="register.php" method="POST" id="ss-form">
+
+<br>
+<div class="errorbox-good">
+
+
+<div class="ss-item ss-item-required ss-text"><div class="ss-form-entry"><label class="ss-q-title" for="entry_0">Name of the Author 1
+<span class="ss-required-asterisk">*:&nbsp;&nbsp;</span></label>
+
+<label class="ss-q-help" for="entry_0"></label>
+<input type="text" name="auth1" value="" size="30" class="ss-q-short" id="entry_0"></div></div></div>
+<br> <div class="errorbox-good">
+
+
+<div class="ss-item ss-item-required ss-text"><div class="ss-form-entry"><label class="ss-q-title" for="entry_1">Name of the Author 2
+<span class="ss-required-asterisk">*:&nbsp;&nbsp</span></label>
+<label class="ss-q-help" for="entry_1"></label>
+<input type="text" name="auth2" value="" size="30" class="ss-q-short" id="entry_1"></div></div></div>
+<br> <div class="errorbox-good">
+
+<div class="ss-item ss-item-required ss-text"><div class="ss-form-entry"><label class="ss-q-title" for="entry_2">Phone Number
+<span class="ss-required-asterisk">*:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span></label>
+
+<label class="ss-q-help" for="entry_2"></label>
+<input type="text" name="mnumber" value=""  size="30"class="ss-q-short" id="entry_2"></div></div></div>
+<br> <div class="errorbox-good">
+    &nbsp;&nbsp;&nbsp;  Place *:
+
+<div class="ss-item ss-item-required ss-text"><div class="ss-form-entry"><label class="ss-q-title" for="entry_7">
+<span class="ss-required-asterisk"></span></label>
+<label class="ss-q-help" for="entry_7"></label>
+
+<textarea rows="4" cols="30" name="address" value=""  class="ss-q-short" id="entry_7"></textarea></div></div></div>
+
+<br> <div class="errorbox-good">
+
+<div class="ss-item ss-item-required ss-checkbox"><div class="ss-form-entry"><label class="ss-q-title" for="entry_3">Events
+<span class="ss-required-asterisk">*</span></label>
+<label class="ss-q-help" for="entry_3"></label>
+<ul class="ss-choices"><li class="ss-choice-item"><label class="ss-choice-label"><input type="checkbox" name="paper" value="Paper" class="ss-q-checkbox" id="group_3_1">
+Paper</label></li> <li class="ss-choice-item"><label class="ss-choice-label"><input type="checkbox" name="poster" value="Poster" class="ss-q-checkbox" id="group_3_2">
+Poster</label></li>
+</ul></div></div></div>
+<br> <div class="errorbox-good">
+<div class="ss-item ss-item-required ss-text"><div class="ss-form-entry"><label class="ss-q-title" for="entry_5">Title
+<span class="ss-required-asterisk">*: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span></label>
+
+<label class="ss-q-help" for="entry_5"></label>
+<input type="text" name="title" value="" size="30" class="ss-q-short" id="entry_5"></div></div></div>
+<br> <div class="errorbox-good">
+<div class="ss-item ss-item-required ss-text"><div class="ss-form-entry"><label class="ss-q-title" for="entry_4">Email address
+<span class="ss-required-asterisk">*: &nbsp;&nbsp;</span></label>
+<label class="ss-q-help" for="entry_4"></label>
+<input type="text" name="email" value="" size="30" class="ss-q-short" id="entry_4"></div></div></div>
+<br> <div class="errorbox-good">
+<div class="ss-item ss-item-required ss-select"><div class="ss-form-entry"><label class="ss-q-title" for="entry_6">Branch
+<span class="ss-required-asterisk">*&nbsp;&nbsp;&nbsp;</span></label>
+<label class="ss-q-help" for="entry_6"></label>
+<select name="branch" id="entry_6"><option value=" "> </option><option value="CSE">CSE</option> <option value="IT">IT</option> <option value="ECE">ECE</option> <option value="EEE">EEE</option> <option value="CIVIL">CIVIL</option> <option value="MEC">MEC</option></select></div></div></div>
+
+<br> <div class="errorbox-good">
+<div class="ss-item ss-item-required ss-radio"><div class="ss-form-entry"><label class="ss-q-title" for="entry_10">Accomidation
+<span class="ss-required-asterisk">*</span></label>
+<label class="ss-q-help" for="entry_10"></label>
+<ul class="ss-choices"><li class="ss-choice-item"><label class="ss-choice-label"><input type="radio" name="acc" value="YES" class="ss-q-radio" id="group_10_1">
+YES</label></li> <li class="ss-choice-item"><label class="ss-choice-label"><input type="radio" name="acc" value="NO" class="ss-q-radio" id="group_10_2">
+NO</label></li>
+</ul></div></div></div>
+<br>
+<input type="hidden" name="pageNumber" value="0">
+<input type="hidden" name="backupCache" value="">
+
+<div class="ss-item ss-navigate"><div class="ss-form-entry">
+
+
+
+<input type="submit" name="submit" value="Submit" >
+</form>
+<script type="text/javascript">
+      
+      (function() {
+var divs = document.getElementById('ss-form').
+getElementsByTagName('div');
+var numDivs = divs.length;
+for (var j = 0; j < numDivs; j++) {
+if (divs[j].className == 'errorbox-bad') {
+divs[j].lastChild.firstChild.lastChild.focus();
+return;
+}
+}
+for (var i = 0; i < numDivs; i++) {
+var div = divs[i];
+if (div.className == 'ss-form-entry' &&
+div.firstChild &&
+div.firstChild.className == 'ss-q-title') {
+div.lastChild.focus();
+return;
+}
+}
+})();
+      </script></div>
+<div class="ss-footer"><div class="ss-attribution"></div>
+<div class="ss-legal"><span class="ss-powered-by">Powered by <a href="http://docs.google.com">Amarnath</a></span>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
